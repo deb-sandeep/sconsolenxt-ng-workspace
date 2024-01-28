60dd883ac4fa80cfd3edf30738acd0bb9c09e4e9
Sandeep Deb <deb.sandeep@gmail.com>
2024-01-28 22:54:13 +0530

    Refactoring to get all files in sync with manual changes

--- projects/app-remote/src/app/app.component.spec.ts
+++ projects/app-remote/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'remote' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('remote');
+    expect(app.title).toEqual('app-remote');
   });
 
   it('should render title', () => {
